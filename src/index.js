6c7486a368a90799732f9e8654f58fc97060d79a
Garvit <garvitgarg200@gmail.com>
2024-08-18 13:23:52 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Note from './components/Note'
 // import express from 'express';
 // import './index.css';
 import App from './App';
-// const app=new express();
-{/* <><h1>hello</h1> <p>tmkc</p></> */}
-
-
 ReactDOM.render(
 <App />
 ,document.getElementById('root'));
